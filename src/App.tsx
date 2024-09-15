130cac4ae82c857e8a8cbbfc4af2d15ac1f9f741
christopher-alden <christopheralden04@gmail.com>
2024-09-15 19:01:40 +0700

    feat: editing still blocked, msal, zustand, rrd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <section></section>
-    </>
+    <section className="w-screen h-screen bg-red-200">
+      <div className="w-full h-full">
+        <a
+          href="https://binusianorg-my.sharepoint.com/personal/christopher_silitonga_binus_ac_id/_layouts/15/guestaccess.aspx?share=EaW86YYYGYZEkt5fV-6Dt8ABheZpH2H6U_abU9TmX7w2WA&e=hD5YfY&action=edit"
+          target="_blank"
+        >
+          <button>Edit in Excel Online</button>
+        </a>
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://binusianorg-my.sharepoint.com/personal/christopher_silitonga_binus_ac_id/_layouts/15/guestaccess.aspx?share=EaW86YYYGYZEkt5fV-6Dt8ABheZpH2H6U_abU9TmX7w2WA&e=ppfSqp&action=embedview&AllowTyping=True&wdInConfigurator=True&wdInConfigurator=True"
+        ></iframe>
+      </div>
+    </section>
   );
 }
 
