786de2d8d0bbb38381daa645e6e2b70b2c57149a
christopher-alden <christopheralden04@gmail.com>
2024-09-15 21:01:35 +0700

    feat: can colab but using docs not microsoft

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -22,7 +22,7 @@ const Work = () => {
         <iframe
           width="100%"
           height="100%"
-          src="https://binusianorg-my.sharepoint.com/personal/christopher_silitonga_binus_ac_id/_layouts/15/guestaccess.aspx?share=EaW86YYYGYZEkt5fV-6Dt8ABheZpH2H6U_abU9TmX7w2WA&e=ppfSqp&action=embedview&AllowTyping=True&wdInConfigurator=True&wdInConfigurator=True"
+          src="https://docs.google.com/spreadsheets/d/1uCJi1WbVO9I72J8VzAsmQol4_NwFjCB_dB4v6WIN7Uo/edit?gid=0#gid=0"
         ></iframe>
       </section>
     </div>
@@ -30,3 +30,9 @@ const Work = () => {
 };
 
 export default Work;
+
+// <iframe
+//   width="100%"
+//   height="100%"
+//   src="https://binusianorg-my.sharepoint.com/personal/christopher_silitonga_binus_ac_id/_layouts/15/guestaccess.aspx?share=EaW86YYYGYZEkt5fV-6Dt8ABheZpH2H6U_abU9TmX7w2WA&e=ppfSqp&action=embedview&AllowTyping=True&wdInConfigurator=True&wdInConfigurator=True"
+// ></iframe>
